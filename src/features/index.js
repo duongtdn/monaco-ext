823ef2df6359d59b93aaa2c675ad8c0592b10ba5
duongtdn <duongtdn@gmail.com>
2025-04-07 14:07:42 +0700

    fix: export Feature

--- src/features/index.js
+++ src/features/index.js
@@ -1,5 +1,6 @@
 "Use strict"
 
+export { default as Feature } from './Feature.interface'
 export { default as LineSelection } from './LineSelection'
 export { default as ReadOnlyLines } from './ReadOnlyLines'
 export { default as AutoResizeHeight } from './AutoResizeHeight'
